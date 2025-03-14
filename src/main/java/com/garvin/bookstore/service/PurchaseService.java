58e7644c8169ff78c7e30ee0e5742a8c1194d310
Garvin Haslett <garvin.haseltt@openbet.com>
2025-03-14 21:47:02 +0000

    Return both values from calculateOutcome

--- src/main/java/com/garvin/bookstore/service/PurchaseService.java
+++ src/main/java/com/garvin/bookstore/service/PurchaseService.java
@@ -1,10 +1,7 @@
 package com.garvin.bookstore.service;
 
 import com.garvin.bookstore.controller.PurchaseController;
-import com.garvin.bookstore.db.BookEntity;
-import com.garvin.bookstore.db.BookRepository;
-import com.garvin.bookstore.db.CustomerEntity;
-import com.garvin.bookstore.db.CustomerRepository;
+import com.garvin.bookstore.db.*;
 import com.garvin.bookstore.model.PurchaseItemModel;
 import com.garvin.bookstore.model.PurchaseModel;
 import com.garvin.bookstore.properties.BookTypeProperties;
@@ -16,6 +13,8 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 @Service
 public class PurchaseService {
@@ -43,7 +42,44 @@ public class PurchaseService {
         }
     }
 
-    public String getPrice(PurchaseModel purchaseModel) {
+    private static boolean isPurchaseInStock(PurchaseItemModel purchaseItemModel, Set<InventoryEntity> inventoryEntities) {
+        Iterator<InventoryEntity> inventoryEntityIterator = inventoryEntities.iterator();
+        while(inventoryEntityIterator.hasNext()){
+            InventoryEntity inventoryEntity = inventoryEntityIterator.next();
+            if (inventoryEntity.getType().equals(purchaseItemModel.getType())) {
+                if (inventoryEntity.getStock() >= purchaseItemModel.getQuantity()) {
+                    return true;
+                } else {
+                    return false;
+                }
+
+            }
+        }
+        return false;
+    }
+
+    class CalculateOutcomeReturnValue {
+        BigDecimal totalCost;
+        long loyaltyPointsBalance;
+
+        public BigDecimal getTotalCost() {
+            return totalCost;
+        }
+
+        public void setTotalCost(BigDecimal totalCost) {
+            this.totalCost = totalCost;
+        }
+
+        public long getLoyaltyPointsBalance() {
+            return loyaltyPointsBalance;
+        }
+
+        public void setLoyaltyPointsBalance(long loyaltyPointsBalance) {
+            this.loyaltyPointsBalance = loyaltyPointsBalance;
+        }
+    }
+
+    private CalculateOutcomeReturnValue calculateOutcome(PurchaseModel purchaseModel) {
         HashMap<String, Long> freeItems = new HashMap<String, Long>();
         for (PurchaseItemModel item: purchaseModel.getFreeItems()) {
             String key = String.format("%s%s", item.getIsbn(), item.getType());
@@ -60,16 +96,20 @@ public class PurchaseService {
                 .map(PurchaseItemModel::getQuantity)
                 .mapToLong(Long::longValue).sum();
         long freeItemsCount = purchaseModel.getFreeItems().stream()
-                        .map(PurchaseItemModel::getQuantity)
-                                .mapToLong(Long::longValue).sum();
+                .map(PurchaseItemModel::getQuantity)
+                .mapToLong(Long::longValue).sum();
         long bundleSize = purchaseItemsCount - freeItemsCount;
         logger.info("bundleSize:" + bundleSize);
 
-        BigDecimal total = BigDecimal.ZERO;
+        BigDecimal totalCost = BigDecimal.ZERO;
         long freeBooksTotal = 0;
         long loyaltyPointsAwarded = 0;
         for (PurchaseItemModel item: purchaseModel.getPurchaseItems()) {
             BookEntity bookEntity = bookRepository.findByIsbn(item.getIsbn());
+
+            if (!isPurchaseInStock(item, bookEntity.getInventory())) {
+                logger.info("There is insufficient stock to meet this order");
+            }
 
             Long purchaseQuantity = item.getQuantity();
             Long freeQuantity = freeItems.getOrDefault(String.format("%s%s", item.getIsbn(), item.getType()), 0L);
@@ -83,9 +123,9 @@ public class PurchaseService {
                     modifier = bundleModifier.get(item.getType());
                 }
 
-                total = total.add(bookEntity.getBasePrice()
-                                .multiply(modifier)
-                                .multiply(BigDecimal.valueOf(purchaseQuantity))
+                totalCost = totalCost.add(bookEntity.getBasePrice()
+                        .multiply(modifier)
+                        .multiply(BigDecimal.valueOf(purchaseQuantity))
                 );
             } else {
                 logger.info("more free copies have been requested than what's available");
@@ -99,7 +139,18 @@ public class PurchaseService {
             logger.info("not enough loyalty points to cover requested free books");
         }
 
-        logger.info("total: {} lpcredit: {} lpdebit: {}", String.valueOf(total), String.valueOf(loyaltyPointsCredit), String.valueOf(loyaltyPointsDebit));
-        return String.valueOf(total);
+        logger.info("totalCost: {} lpcredit: {} lpdebit: {}", String.valueOf(totalCost), String.valueOf(loyaltyPointsCredit), String.valueOf(loyaltyPointsDebit));
+
+        CalculateOutcomeReturnValue calculateOutcomeReturnValue = new CalculateOutcomeReturnValue();
+        calculateOutcomeReturnValue.setTotalCost(totalCost);
+        calculateOutcomeReturnValue.setLoyaltyPointsBalance(loyaltyPointsCredit - loyaltyPointsDebit);
+        return calculateOutcomeReturnValue;
+    }
+
+    public String getPrice(PurchaseModel purchaseModel) {
+        CalculateOutcomeReturnValue calculateOutcomeReturnValue = calculateOutcome(purchaseModel);
+        return String.format("Total cost: %s, Loyalty Point balance: %s",
+                String.valueOf(calculateOutcomeReturnValue.getTotalCost()),
+                String.valueOf(calculateOutcomeReturnValue.getLoyaltyPointsBalance()));
     }
 }
